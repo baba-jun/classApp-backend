3b0d804173b39442c4dfbc7db3ddfe86172b38d1
baba-jun <msk3059i@gmail.com>
2024-12-10 00:11:51 +0900

    CORS対策

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
+
+	// CORSミドルウェアの設定
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
+	}))
 
 	// ルートハンドラの定義
 	router.GET("/", func(c *gin.Context) {
